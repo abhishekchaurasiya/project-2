c2a83fbfcb47c2c2966120ff13700b29ad12c9a5
abhishekchaurasiya <abhichek2580@gmail.com>
2022-05-06 11:44:44 +0530

    comment

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,10 @@ const { collegeName } = require('../controller/collegeController');
 const { internDetails , getInternDetails} = require("../controller/internController")
 
 
+// college details api
 router.post("/functionup/colleges", collegeName);
 
+// interns details api
 router.post("/functionup/interns", internDetails);
 
 router.get("/functionup/collegeDetails", getInternDetails)
